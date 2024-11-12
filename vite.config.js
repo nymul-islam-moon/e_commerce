44c6cce91d8c9a2def391d8f19765046eac2b21a
shamsalm0 <shamsalm0@gmail.com>
2024-11-13 01:11:38 +0600

    mastering the landing page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/plugins/bootstrap.min.css',
+                'resources/css/plugins/flaticon.css',
+                'resources/css/plugins/font-awesome.css',
+                'resources/css/plugins/iconfont.css',
+                'resources/css/plugins/ion.rangeSlider.min.css'
             ],
             refresh: true,
         }),
