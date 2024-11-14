c3a1619eb26f82b5d57bd41448d36be846ab75e6
Nymul Islam Moon <towkir1997islam@gmail.com>
2024-11-14 21:18:45 +0600

    file structure rearrange

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Frontend;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -9,6 +9,6 @@ class HomeController extends Controller
 {
     public function index() {
 
-        return view('frontend.index');
+        return view('index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
